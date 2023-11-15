2e4a625bfaf6d9547b3d92d25d334966b210a6ee
Bruno Macedo <macedobruno@id.uff.br>
2023-11-15 13:49:37 -0300

    Keybinds for environments where super key is not available (shit environments)

--- evil-and-keybinds-config.el
+++ evil-and-keybinds-config.el
@@ -18,3 +18,33 @@
 (evil-define-key '(normal insert visual replace) global-map (kbd "M-<SPC>") 'company-complete)
 
 (evil-define-key 'insert global-map (kbd "C-y") 'yank)
+
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x 1") 'delete-other-windows)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x 2") 'split-window-below)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x 3") 'split-window-right)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x 0") 'delete-window)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x i") 'toggle-index-buffer)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x l") 'brnm-toggle-link-buffer)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x L") 'brnm-run-link)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x b") 'switch-to-buffer)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x o") 'other-window)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x c") 'toggle-chromium-buffer)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x j") 'next-buffer)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x k") 'previous-buffer)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x K") 'kill-this-buffer)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x t") 'brnm-toggle-vterm-buffer)
+;;
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x -") 'shrink-window)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x _") 'shrink-window-horizontally)
+
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x r") 'exwm-reset)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x w") 'exwm-workspace-switch)
+(evil-define-key '(normal insert visual replace) global-map (kbd "C-x &") '(lambda (command)
+									     (interactive (list (read-shell-command "$ ")))
+									     (start-process-shell-command command nil command)))
