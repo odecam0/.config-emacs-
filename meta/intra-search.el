842a00ecd1681f885d3f10fc886b366744e37fce
Bruno Macedo <macedobruno@id.uff.br>
2024-02-03 15:16:22 -0300

    Functionality of intra-search is now toggleble and default is off

--- meta/intra-search.el
+++ meta/intra-search.el
@@ -2,16 +2,30 @@
 ;; ( on...
 ;; This should be made toggleble
 ;; make search-forward skip invisible overlay text
+
 (defun brnm-make-skip-invisible (&rest r)
   ()
   (if (not (string-equal (substring (car r) 0 1) "("))
       (while (invisible-p (point))
 	(apply 'search-forward r))))
-(advice-add 'search-forward :after 'brnm-make-skip-invisible)
-;; (advice-remove 'search-forward 'brnm-make-skip-invisible)
 
+(defvar brnm-skip-invisible-activep nil
+  "Is true when search forward is advised by brnm-make-skip-invisible function")
 
-(advice-add 'ee-goto-position :before '_mc-turn-on-overlays)
-(advice-add 'find-fline :after '_mc-turn-off-overlays)
+(defun brnm-toggle-skip-invisible ()
+  (interactive)
+  (if brnm-skip-invisible-activep
+      (progn
+	(advice-remove 'search-forward 'brnm-make-skip-invisible)
+	(advice-remove 'ee-goto-position '_mc-turn-on-overlays)
+	(advice-remove 'find-fline '_mc-turn-off-overlays)
+	(setq brnm-skip-invisible-activep nil))
+    (advice-add 'search-forward :after 'brnm-make-skip-invisible)
+    (advice-add 'ee-goto-position :before '_mc-turn-on-overlays)
+    (advice-add 'find-fline :after '_mc-turn-off-overlays)
+    (setq brnm-skip-invisible-activep t)))
+
+(defalias 'btsi 'brnm-toggle-skip-invisible)
+
 ;;(advice-remove 'ee-goto-position '_mc-turn-on-overlays)
 ;;(advice-remove 'find-fline '_mc-turn-off-overlays)
