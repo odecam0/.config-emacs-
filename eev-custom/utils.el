0706a2ad26f4ac69221ff4d71ecff13582dbe10f
Bruno Macedo <macedobruno@id.uff.br>
2024-02-15 11:21:44 -0300

    Added functionality for pdf pages hyperlinks, and minor-mode

--- eev-custom/utils.el
+++ eev-custom/utils.el
@@ -9,6 +9,7 @@
   (interactive)
   (save-excursion
     (beginning-of-line)
+    (search-forward "(")
     (search-forward " ")
     (let ((which-hlink (buffer-substring (- (point) 5) (- (point) 1))))
       (delete-region (- (point) 5) (- (point) 1))
@@ -17,4 +18,36 @@
       (if (string= which-hlink "page") (insert "text"))
       (if (string= which-hlink "text") (insert "page")))))
 
-(evil-define-key 'normal global-map (kbd "M-b") 'brnm-toggle-page-text)
+(defun brnm-increase-page-number ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (search-forward-regexp "-?[0-9]+")
+    (let (number)
+      (setq number (string-to-number (match-string 0)))
+      (delete-region (match-beginning 0) (match-end 0))
+      (insert (number-to-string (+ 1 number))))))
+
+(defun brnm-decrease-page-number ()
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (search-forward-regexp "-?[0-9]+")
+    (let (number)
+      (setq number (string-to-number (match-string 0)))
+      (delete-region (match-beginning 0) (match-end 0))
+      (insert (number-to-string (+ -1 number))))))
+
+;; (find-node "(*Apropos*)Index for ‘emacs lisp’")
+;;  (info "(elisp)Minor Modes")
+;;  (find-node "(elisp)Defining Minor Modes" "Macro: define-minor-mode mode doc keyword-args... body...")
+;;  (find-node "(elisp)Defining Minor Modes" ":keymap KEYMAP")
+
+(setq page-utils-mode-map (make-sparse-keymap))
+(evil-define-key '(normal insert) page-utils-mode-map (kbd "M-b") #'brnm-toggle-page-text)
+(evil-define-key '(normal insert) page-utils-mode-map (kbd "M-n") #'brnm-increase-page-number)
+(evil-define-key '(normal insert) page-utils-mode-map (kbd "M-p") #'brnm-decrease-page-number)
+
+(define-minor-mode page-utils-mode
+  "Defines a keymap for interacting and modifying pdf hyperlinks with simple commands.")
+
